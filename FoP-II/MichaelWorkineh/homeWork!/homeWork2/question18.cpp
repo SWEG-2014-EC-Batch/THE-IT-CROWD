f4b54631c8d53e9bb8f29c6df7a57acb486c7011
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2023-06-19 11:06:22 +0300

    Update question18.cpp

--- FoP-II/MichaelWorkineh/homeWork!/homeWork2/question18.cpp
+++ FoP-II/MichaelWorkineh/homeWork!/homeWork2/question18.cpp
@@ -6,6 +6,12 @@ void discountCalculator(double &price, double discount, bool discountType);
 int main(){
     double price, discount;
     bool discountType;
+    cout<<"enter the price"<<endl;
+cin>>price;
+cout<<"enter the discount rate"<<endl;
+cin>>discount;
+cout<<"type 1 if the discount is in percentage and type 2 if not"<<endl;
+cin>>discountType;
     discountCalculator(price, discount, discountType);
     if(price>0){
         cout<<"the discount price is "<<price<<endl;
