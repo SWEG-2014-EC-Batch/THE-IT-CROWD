5b55d3671d995d58514d78235e5f4b892ed4990b
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2023-06-19 11:08:29 +0300

    Update exersice15.cpp

--- FoP-II/MichaelWorkineh/homeWork!/homeWork2/exersice15.cpp
+++ FoP-II/MichaelWorkineh/homeWork!/homeWork2/exersice15.cpp
@@ -13,9 +13,9 @@ int main(){
     cin>>num3;
     if(num1!=0){
         scale(num1,num2,num3);
-        cout<<"the old value of number 2 is "<<num3<<endl;
+        cout<<"the old value of number 2 is "<<num2/num1<<endl;
         cout<<"the new  value number2 is "<<num2<<endl;
-        cout<<"the old value of number 3 is "<<num3<<endl;
+        cout<<"the old value of number 3 is "<<num2/num1<<endl;
         cout<<"the new  value number3 is "<<num3<<endl;
         (scalechecker==0)?cout<<"they have been scaled down"<<endl:cout<<"they have been scaled up"<<endl;
     }
