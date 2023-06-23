202bb6eb286bc9afcdf2f85e82cbd3b2b91e3a7d
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2023-06-23 00:09:08 -0700

    resolving conflict while pushing this weeks codes

--- FoP-II/MichaelWorkineh/homeWork!/homeWork2/question18.cpp
+++ FoP-II/MichaelWorkineh/homeWork!/homeWork2/question18.cpp
@@ -6,15 +6,12 @@ void discountCalculator(double &price, double discount, bool discountType);
 int main(){
     double price, discount;
     bool discountType;
-<<<<<<< HEAD
-=======
     cout<<"enter the price"<<endl;
 cin>>price;
 cout<<"enter the discount rate"<<endl;
 cin>>discount;
 cout<<"type 1 if the discount is in percentage and type 2 if not"<<endl;
 cin>>discountType;
->>>>>>> ccd727f092856b0ef50838f5c061b8ce421d5b9b
     discountCalculator(price, discount, discountType);
     if(price>0){
         cout<<"the discount price is "<<price<<endl;
@@ -24,13 +21,10 @@ cin>>discountType;
 
 void discountCalculator(double &price, double discount, bool discountType){
     if(discountType==1){
-        price=price-price*discount;
+        price=price-price*discount/100;
     }
     else{
-        price=price-price*discount;
+        price=price-discount;
     }
-<<<<<<< HEAD
 }
-=======
 }
->>>>>>> ccd727f092856b0ef50838f5c061b8ce421d5b9b
