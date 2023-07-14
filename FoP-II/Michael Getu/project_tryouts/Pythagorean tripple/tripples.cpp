87b1a2f4617d8cb1d5036f099f58b9463e7d2452
MichaelG <97118715+MichaelGetu-git@users.noreply.github.com>
2023-07-14 13:13:43 +0300

    Create tripples.cpp

---
+++ FoP-II/Michael Getu/project_tryouts/Pythagorean tripple/tripples.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include"local_lib2.h"
+using namespace std;
+int main() {
+  int choice;
+
+  do {
+    printMenu();
+    choice = getChoice();
+
+    if (choice == 1) {
+      printPythagoreanTable();
+    } else if (choice == 2) {
+      inputAndPrintTriangle();
+    }
+
+  } while (choice != 3);
+
+  return 0;
+}
