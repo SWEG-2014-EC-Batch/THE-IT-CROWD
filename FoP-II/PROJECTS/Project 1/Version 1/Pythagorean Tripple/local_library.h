32765a4025969506cbbc7d235c76d06a23d90dc4
MichaelG <97118715+MichaelGetu-git@users.noreply.github.com>
2023-07-15 16:38:35 +0300

    Update local_library.h

--- FoP-II/PROJECTS/Project 1/Version 1/Pythagorean Tripple/local_library.h
+++ FoP-II/PROJECTS/Project 1/Version 1/Pythagorean Tripple/local_library.h
@@ -1,1 +1,30 @@
+#ifndef LOCAL_LIB_H
+#define LOCAL_LIB_H
+#include<iostream>
+using namespace std;
+// Triangle structure
+struct Triangle {
+  int side1;
+  int side2;
+  int hypotenuse;
+};
 
+//2
+
+//3
+
+//4
+
+
+// Print triangle sides
+void printTriangle(Triangle t) {
+  cout << "Side 1: " << t.side1 << endl;
+  cout << "Side 2: " << t.side2 << endl;
+  cout << "Hypotenuse: " << t.hypotenuse << endl;
+}
+
+// Check Pythagorean triangle property
+bool isPythagorean(Triangle t) {
+  return t.side1 * t.side1 + t.side2 * t.side2 == t.hypotenuse * t.hypotenuse;
+}
+#endif
