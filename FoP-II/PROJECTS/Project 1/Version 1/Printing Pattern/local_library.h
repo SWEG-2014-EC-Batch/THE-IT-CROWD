233c8777fed655d6af70752caeb14c9862f4c792
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-07-15 20:20:15 +0300

    Update local_library.h

--- FoP-II/PROJECTS/Project 1/Version 1/Printing Pattern/local_library.h
+++ FoP-II/PROJECTS/Project 1/Version 1/Printing Pattern/local_library.h
@@ -8,7 +8,6 @@ void printFirstHalf(int n);
 void printSecondHalf(int n);
 
 //1
-// Function to print a sequence of spaces multiple times
 void printChars(char ch, int count) {
     for (int i = 0; i < count; i++) {
         cout << "  ";
@@ -30,9 +29,18 @@ void printFirstHalf(int n) {
 }
 
 //3
-
-
-
+void printSecondHalf(int n) {
+    for (int i = 2; i <= n; i++) {
+        printChars(' ', n - i);
+        for (int j = i; j >= 1; j--) {
+            cout << char('A' + j - 1) << ' ';
+        }
+        for (int j = 2; j <= i; j++) {
+            cout << char('A' + j - 1) << ' ';
+        }
+        cout << endl;
+    }
+}
 
 #endif
 
