83db40d7546988af864db61a65dec8014965ee82
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-07-15 20:21:34 +0300

    Update local_library.h

--- FoP-II/PROJECTS/Project 1/Version 1/Printing Pattern/local_library.h
+++ FoP-II/PROJECTS/Project 1/Version 1/Printing Pattern/local_library.h
@@ -29,18 +29,8 @@ void printFirstHalf(int n) {
 }
 
 //3
-void printSecondHalf(int n) {
-    for (int i = 2; i <= n; i++) {
-        printChars(' ', n - i);
-        for (int j = i; j >= 1; j--) {
-            cout << char('A' + j - 1) << ' ';
-        }
-        for (int j = 2; j <= i; j++) {
-            cout << char('A' + j - 1) << ' ';
-        }
-        cout << endl;
-    }
-}
+
+
 
 #endif
 
