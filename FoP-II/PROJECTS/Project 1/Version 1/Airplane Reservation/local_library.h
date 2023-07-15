705ea5797e8488b0cd861b68674d7af2c384b3c9
MichaelG <97118715+MichaelGetu-git@users.noreply.github.com>
2023-07-15 16:52:03 +0300

    Update local_library.h

--- FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/local_library.h
+++ FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/local_library.h
@@ -1,1 +1,47 @@
+#ifndef LOCAL_LIB_H
+#define LOCAL_LIB_H
+#include<iostream>
+using namespace std;
 
+const int MAX_FIRST_CLASS = 30;
+const int MAX_ECONOMY = 100;
+
+
+//2
+
+//3
+
+
+
+void bookFlight(Flight& flight) {
+  cout << endl;
+  // Prompt the user for passenger details
+  string name, passportNumber;
+  char sex;
+  int age;
+  cout << "Enter passenger name: ";
+  cin.ignore();
+  getline(cin, name);
+  cout << "Enter passenger sex (m/f): ";
+  cin >> sex;
+  cout << "Enter passenger age: ";
+  cin >> age;
+  cout << "Enter passport number: ";
+  cin >> passportNumber;
+  cout << "Select class (1 - First Class, 2 - Economy): ";
+  int classChoice;
+  cin >> classChoice;
+
+  // Create the passenger object
+  Passenger passenger{name, sex, age, passportNumber};
+
+  // Add the passenger to the flight
+  addPassenger(flight, passenger, classChoice);
+}
+
+//4
+
+
+
+
+#endif
