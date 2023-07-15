6f60ef617c95cf49538f11561d723b0e1c627737
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-07-15 20:15:44 +0300

    Update local_library.h

--- FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/local_library.h
+++ FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/local_library.h
@@ -106,11 +106,41 @@ void bookFlight(Flight& flight) {
   addPassenger(flight, passenger, classChoice);
 }
 
+void searchPassport(Flight& flight) {
+  cout << endl;
+  // Prompt the user for the passport number to search
+  string passportNumber;
+  cout << "Enter passport number to search: ";
+  cin >> passportNumber;
 
+  // Search for the passenger
+  Passenger* passenger = getPassenger(flight, passportNumber);
+  if (passenger != nullptr) {
+    cout << "\nPassport Found!\n" << endl;
+    cout << "Name: " << passenger->name << endl;
+    cout << "Sex: " << passenger->sex << endl;
+    cout << "Age: " << passenger->age << endl;
+    cout << "Passport Number: " << passenger->passportNumber << endl;
+    cout << "Seat No: ";
+    if (passenger >= flight.firstClass && passenger < flight.firstClass + MAX_FIRST_CLASS) {
+      cout << (passenger - flight.firstClass) + 1 << " (First Class)" << endl;
+    } else {
+      cout << (passenger - flight.economyClass) + 1 << " (Economy Class)" << endl;
+    }
+  } else {
+    cout << "\nPassport Not Found!" << endl;
+  }
+}
 
-//4
+void printPassengerss(Flight& flight) {
+  cout << endl;
+  if (flight.firstClassCount == 0 && flight.economyClassCount == 0) {
+    cout << "No passengers found." << endl;
+    return;
+  }
 
-
-
+  cout << "List of Passengers:" << endl;
+  printPassengers(flight);
+}
 
 #endif
