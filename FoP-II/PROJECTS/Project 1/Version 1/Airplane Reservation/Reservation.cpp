21c4f2df960ae81b15132fde7537b44d445044a1
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-07-15 20:16:29 +0300

    Update Reservation.cpp

--- FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/Reservation.cpp
+++ FoP-II/PROJECTS/Project 1/Version 1/Airplane Reservation/Reservation.cpp
@@ -1,1 +1,32 @@
-//4
+#include <iostream>
+#include <string>
+#include "local_lib.h"
+
+using namespace std;
+
+int main() {
+  Flight flight;
+  while (true) {
+    showMenu();
+    cout << "Enter your choice: ";
+    int choice = getChoice();
+
+    switch (choice) {
+      case 1:
+        bookFlight(flight);
+        break;
+      case 2:
+        searchPassport(flight);
+        break;
+      case 3:
+        printPassengerss(flight);
+        break;
+      case 4:
+        return 0;
+      default:
+        cout << "Invalid input. Please try again." << endl;
+    }
+  }
+
+  return 0;
+}
