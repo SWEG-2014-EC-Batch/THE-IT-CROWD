f1c978043f0ed6267b75e5eb53be1c1b47ad55d8
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2023-07-15 10:17:10 -0700

    Update Phthagorran_Tripple.cpp

--- FoP-II/PROJECTS/Project 1/Version 1/Pythagorean Tripple/Phthagorran_Tripple.cpp
+++ FoP-II/PROJECTS/Project 1/Version 1/Pythagorean Tripple/Phthagorran_Tripple.cpp
@@ -1,1 +1,22 @@
-//1
+#include <iostream>
+#include <cmath>
+#include"local_lib.h"
+using namespace std;
+int main() {
+  int choice;
+
+  do {
+    printMenu();
+    choice = getChoice();
+
+    if (choice == 1) {
+      printPythagoreanTable();
+    } else if (choice == 2) {
+      inputAndPrintTriangle();
+    }
+
+  } while (choice != 3);
+
+  return 0;
+}
+
